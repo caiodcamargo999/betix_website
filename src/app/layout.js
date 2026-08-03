dd86f081b563645270723c21bbe87979250b6988
Caio Camargo <caiorarity@gmail.com>
2026-08-03 18:23:34 -0300

    Update Meta Pixel ID in layout.js

--- src/app/layout.js
+++ src/app/layout.js
@@ -30,7 +30,7 @@ n.queue=[];t=b.createElement(e);t.async=!0;
 t.src=v;s=b.getElementsByTagName(e)[0];
 s.parentNode.insertBefore(t,s)}(window, document,'script',
 'https://connect.facebook.net/en_US/fbevents.js');
-fbq('init', '2576042539502638');
+fbq('init', '1634810871771318');
 fbq('track', 'PageView');`,
           }}
         />
@@ -46,7 +46,7 @@ fbq('track', 'PageView');`,
         </noscript>
         <noscript>
           <img height="1" width="1" style={{ display: "none" }}
-            src="https://www.facebook.com/tr?id=2576042539502638&ev=PageView&noscript=1"
+            src="https://www.facebook.com/tr?id=1634810871771318&ev=PageView&noscript=1"
           />
         </noscript>
         {children}
